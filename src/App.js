e6ff733318faa04dbb9df2e8abde1966d6028324
Victoria Kronsell <victoriakronsell@Victorias-MacBook-Pro-2.local>
2019-11-05 08:40:08 +1000

    App spec stuff

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
+import ButtonExamples from './components/button/examples';
+import { ThemeProvider } from 'styled-components';
+import Select from 'react-select';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Theming
+import defaultBrand from './themes/defaultBrand';
+import brand1 from './themes/brand1';
+import brand2 from './themes/brand2';
+
+const options = [
+  {value: 'defaultBrand', label: 'Default brand'},
+  {value: 'brand1', label: 'Brand 1'},
+  {value: 'brand2', label: 'Brand 2'},
+]
+
+const themes = {
+  defaultBrand: defaultBrand,
+  brand1: brand1,
+  brand2: brand2
+}
+
+console.log(defaultBrand);
+
+
+class App extends React.Component {
+  state = {
+    selectedOption: options[0],
+  }
+  handleChange = selectedOption => {
+    this.setState(
+      { selectedOption },
+      () => console.log(`Option selected:`, this.state.selectedOption)
+    );
+  };
+  render() {
+    const { selectedOption } = this.state;
+    console.log(selectedOption.label);
+
+    return (
+      <div className="app">
+        <h1>Select brand (for demo)</h1>
+        <Select
+          value={selectedOption}
+          onChange={this.handleChange}
+          options={options}
+      />
+      <hr/>
+
+        <h1>Displaying: {selectedOption.label}</h1>
+        <ThemeProvider theme={themes[selectedOption.value]}>
+            <ButtonExamples/>
+        </ThemeProvider>
+      </div>
+    );
+    }
 }
 
 export default App;
